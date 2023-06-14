7bc4927d9a3daff96abd3de1f08696510b3bf34c
Kyamu <kyamuddin352@gmail.com>
2023-06-14 07:27:28 +0530

    basic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+
+
+
+const App = () => {
+
+  let [cord, setCord] = useState({ latitude: "", longitude: "" });
+  let [hemisphere, setHemisphere] = useState("");
+  let [month, setMonth] = useState(new Date().getMonth());
+
+  let monthArr = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "Octuber", "November", "December"];
+
+
+  let { latitude, longitude } = cord
+
+  function findLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        console.log(position);
+        setCord({ latitude: position.coords.latitude, longitude: position.coords.longitude })
+        findHemisphere(position.coords.latitude);
+      })
+    }
+  }
+
+  function findHemisphere(latitude) {
+    if (latitude > 0) {
+      setHemisphere("Northern Hemisphere")
+    }
+    else if (latitude < 0) {
+      setHemisphere("Southern Hemisphere")
+    }
+    else {
+      setHemisphere("Equator")
+    }
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Latitude:{latitude},Longitude:{longitude}</h1>
+      <button onClick={findLocation}>Find Location</button>
+      <h1>{hemisphere}</h1>
+      <h1>{monthArr[month]}</h1>
     </div>
-  );
+  )
 }
 
 export default App;
